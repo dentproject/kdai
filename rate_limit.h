cf594e2258e4c035816f43171df1953b0101d5b4
Korel <Korelucpinar@gmail.com>
2025-05-09 20:09:49 -0400

    Switch from Static Maximum Interfaces to Dynamic Memory Allocation with spinlock protection
    Each rate limit entry is now created at runtime using kmalloc and inserted into a global list. This change is meant to enhance scalability and decrease the configuration dependency requirements.
    
    Signed-off-by: Korel <Korelucpinar@gmail.com>

--- rate_limit.h
+++ rate_limit.h
@@ -1,5 +1,13 @@
 #include "common.h"
 
+struct rate_limit_entry {
+    char iface_name[IFNAMSIZ];  // Store the interface name
+    unsigned int packet_count;  // Count of packets received
+    unsigned long last_packet_time;  // Time of the last packet received
+    struct list_head list;
+
+};
+
 // Function declarations
 bool rate_limit_reached(struct  sk_buff* skb);
 void clean_rate_limit_table(void);
