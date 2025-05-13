97e3408123823ebd7dd51a9afe637dea34cce393
Korel <Korelucpinar@gmail.com>
2025-05-13 10:55:24 -0700

    Make trusted interface lookup VLAN-aware
    Trusted and untrusted interfaces must now be associated with a specific VLAN when added.  The function find_trusted_interface() now takes a VLAN ID as a parameter. It only returns true if the given interface name exists and matches the specified VLAN ID. This ensures trust decisions are scoped correctly to the VLAN. For example, a trusted interface on VLAN 100 is not automatically trusted on VLAN 200. As a result, callers (e.g. in main.c) must now extract the VLAN ID from the packet and pass it into find_trusted_interface().
    
    Signed-off-by: Korel <Korelucpinar@gmail.com>

--- trustedInterfaces.h
+++ trustedInterfaces.h
@@ -5,15 +5,16 @@ struct interface_entry {
     char name[IFNAMSIZ];    // Store trusted interface name
                             // IFNAMSIZ is a constant in the linux kernel
                             // that defines the maximum length of a network interface
+    u16 vlan_id;            //The vlan associated with DAI
     struct list_head list;  // Point to the next item in the linked list 
 };
 
 // Function declarations
 void populate_trusted_interface_list(void);
 
-int insert_trusted_interface(const char *device_name);
+int insert_trusted_interface(const char *device_name, u16 vlan_id);
 
-const char* find_trusted_interface(const char *interface_name);
+const char* find_trusted_interface(const char *interface_name, u16 vlan_id);
 
 void print_trusted_interface_list(void);
 
