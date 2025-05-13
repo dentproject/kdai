2c2991a4d5b33c4fb84e46d355e7e02859968bc7
Korel <Korelucpinar@gmail.com>
2025-05-13 10:50:41 -0700

    Implement VLAN inspection hash table with add/remove operations
    Created a hash table to store the VLANs to be inspected. Support functions to add, check, and remove VLANs from the inspection list are included.
    
    Signed-off-by: Korel <Korelucpinar@gmail.com>

---
+++ vlan.h
@@ -0,0 +1,16 @@
+#include "common.h"
+
+struct vlan_hash_entry {
+    u16 vlan_id;
+    struct hlist_node node;
+};
+
+void add_vlan_to_inspect(u16 vlan_id);
+
+bool vlan_should_be_inspected(u16 vlan_id);
+
+void remove_vlan_from_inspect(u16 vlan_id);
+
+void print_all_vlans_in_hash(void);
+
+void init_vlan_hash_table(void);
